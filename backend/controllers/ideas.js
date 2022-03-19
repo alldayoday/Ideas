3f5c65a65b0536492cdc785d6392c5263c779a69
RMo <morici23@gmail.com>
2022-03-19 15:43:11 -0400

    Write controller functions

--- backend/controllers/ideas.js
+++ backend/controllers/ideas.js
@@ -13,18 +13,30 @@ const index = (req, res) => {
 
 const create = (req, res) => {
   console.log('create')
+  Idea.create(req.body)
+  .then(idea => res.json(idea))
+  .catch(error => res.json(error))
 }
 
 const deleteIdea = (req, res) => {
   console.log('deleteIdea')
+  Idea.findByIdAndDelete(req.params.id)
+  .then(idea => res.json(idea))
+  .catch(error => res.json(error))
 }
 
 const update = (req, res) => {
   console.log('update')
+  Idea.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(idea => res.json(idea))
+  .catch(error => res.json(error))
 }
 
 const show = (req, res) => {
   console.log('show')
+  Idea.findById(req.params.id)
+  .then(idea => res.json(idea))
+  .catch(error => res.json(error))
 }
 
 export {
